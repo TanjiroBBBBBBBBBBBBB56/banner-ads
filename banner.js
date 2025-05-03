4234359d64019a537ef3e14cf09a05e215561bf6
TanjiroBBBBBBBBBBBBB56 <bhempire576@gmail.com>
2025-05-04 04:38:03 +0600

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,11 @@
+// banner.js
+const adScript = document.createElement("script");
+adScript.async = true;
+adScript.dataset.cfasync = "false";
+adScript.src = "//pl26451889.profitableratecpm.com/f4cd4a124c226bef04b12977ae75c00f/invoke.js";
+document.body.appendChild(adScript);
+
+// Add the container where the ad will appear
+const container = document.createElement("div");
+container.id = "container-f4cd4a124c226bef04b12977ae75c00f";
+document.body.appendChild(container);
